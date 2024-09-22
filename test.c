751c57313fbdf64e055ff7072b3f854b4f1c2777
Daxrokz10 <dakshgagnani@gmail.com>
2024-09-22 15:40:32 +0530

    hg

--- test.c
+++ test.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hello World");
+    int num1,num2,sum;
+    printf("enter num1:");
+    scanf("%d",&num1);
+
+    printf("enter num2:");
+    scanf("%d",&num2);
+
+    sum = num1+ num2;
+    printf("%d",sum);
+
+    return 0;
 }
