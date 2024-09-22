6a3a9afb8046e7d14e7e955e36325f1e336ec025
Daxrokz10 <dakshgagnani@gmail.com>
2024-09-22 21:22:46 +0530

    string functions

--- test.c
+++ test.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
-
+#include<string.h>
 int main(){
-    int num1,num2,sum;
-    printf("enter num1:");
-    scanf("%d",&num1);
+    // int num1,num2,sum;
+    // printf("enter num1:");
+    // scanf("%d",&num1);
 
-    printf("enter num2:");
-    scanf("%d",&num2);
+    // printf("enter num2:");
+    // scanf("%d",&num2);
 
-    sum = num1+ num2;
-    printf("%d",sum);
+    // sum = num1+ num2;
+    // printf("%d",sum);
+    // return 0;
+
+    char str [] = "Daksh";
+    printf("String normal is: %s \n",str);
+    printf("String reversed is: %s",strrev(str));
 
     return 0;
 }
