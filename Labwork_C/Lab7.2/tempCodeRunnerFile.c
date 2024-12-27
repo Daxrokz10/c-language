61eb0ddae8733f617aec76c7a9811b73eb75aab5
Daxrokz10 <dakshgagnani@gmail.com>
2024-12-27 10:52:51 +0530

    kajukatli done

--- Labwork_C/Lab7.2/tempCodeRunnerFile.c
+++ Labwork_C/Lab7.2/tempCodeRunnerFile.c
@@ -1,14 +1,3 @@
-#include <stdio.h>
-
-int main(){
-    int i,j,s;
-    for(i=1;i<=5;i++){
-        for(s = 5; s >= i + 1; s--){
-            printf("  ");
-        }
-        for(j=5;j>=i;j--){
-            printf("%d ",j);
-        }
-        printf("\n");
-    }
-}
+ // for(j=1;j<=i;j++){
+        //     printf("%d ",j);
+        // }
