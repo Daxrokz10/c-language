e7836a1d40a26bc906ad98438a800db0fa592954
Daxrokz10 <dakshgagnani@gmail.com>
2024-12-31 12:06:16 +0530

    work

--- Project/PR-3 Looper/Q3_digit_adder.c
+++ Project/PR-3 Looper/Q3_digit_adder.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h> // For isdigit function
 
 int main() {
-    char num[100]; 
+    char num[100];
     int first_digit, last_digit, sum;
 
-   
+    // Prompt user for input
     printf("Enter a number: ");
     scanf("%s", num);
 
-
-    first_digit = num[0]; //number one is the first digit of the number
-    last_digit = num[0]; //temporary value assigned to lastdigit
-
-
-    for (int i = 0; i < strlen(num); i++) {  //use strlen to find length of number
-        if (i == strlen(num) - 1) {//when the number of the string is last store that value in last digit
-            last_digit = num[i];    
+    // Validate input to ensure it's a number
+    for (int i = 0; i < strlen(num); i++) {
+        if (!isdigit(num[i])) {
+            printf("Invalid input. Please enter a valid number.\n");
+            return 1; // Exit the program if input is invalid
         }
     }
 
- 
-    sum = first_digit + last_digit;//sum and print
+    // Convert first and last characters to integers
+    first_digit = num[0] - '0'; // Convert ASCII to integer
+    last_digit = num[strlen(num) - 1] - '0'; // Convert ASCII to integer
 
+    // Calculate the sum
+    sum = first_digit + last_digit;
 
+    // Print the result
     printf("Sum of the first and last digit: %d\n", sum);
 
     return 0;
